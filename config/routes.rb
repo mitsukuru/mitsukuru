0b7bf6c365e01e66d7d5585cc448efeeb12ccf93
GOTO M <88922437+masaa0802@users.noreply.github.com>
2025-07-06 22:22:48 +0900

    delete: test画面削除

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
       resources :posts
       resources :users
       get "/me" => "users#me"
-      get "/test" => "test#index"
       post "/callback" => "oauths#callback"
       get "/callback" => "oauths#callback"
       get "/:provider" => "oauths#oauth", as: :auth_at_provider
